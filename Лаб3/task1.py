017cd2a2e7ac79e31c0dfbe9c53dc459ed974961
alenichch13 <148383180+alenichch13@users.noreply.github.com>
2023-10-29 23:59:39 +0300

    Add files via upload

---
+++ Лаб3/task1.py
@@ -0,0 +1,16 @@
+# TODO Напишите функцию для поиска индекса товара
+
+def poisk(item_list, item):
+    if item in item_list:
+        return item_list.index(item)
+    else:
+        return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = poisk(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
